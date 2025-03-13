1cf98ead92d6373c47a577d7835ed5da697ff416
mihkuno <caindayjoeninyo@gmail.com>
2025-03-13 23:13:58 +0800

    rename lib chroma to database

--- src/lib/ollama/main.ts
+++ src/lib/ollama/main.ts
@@ -1,6 +1,6 @@
 import ollama from 'ollama';
 import { Message } from 'ollama';
-import { Response } from '@types/chroma';
+import { Response } from '@/types/database';
 import { Coherence } from '@/types/coherence';
 
 async function fetchDatabase(query: string): Promise<string> {
